3d49e4ed031f497909186b9fc70408afb8f33a19
angelotax <angelotax@gmail.com>
2023-06-27 17:08:30 +0200

    platform switch

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 "use strict";
 const sectionCenter = document.querySelector(".section-center");
 const ButtonContainer = document.querySelector(".btn-container");
-
-const request = fetch(
-  "https://api.rawg.io/api/games?genres=arcade&key=73601ec88eab474386a6952aa8b34734"
-  // "https://api.rawg.io/api/games?key=73601ec88eab474386a6952aa8b34734&dates=2019-09-01,2019-09-30&platforms=18,1,"
+// request platform
+const requestPlatform = fetch(
+  "https://api.rawg.io/api/platforms?key=73601ec88eab474386a6952aa8b34734"
 )
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
-    const gameArticles = data.results;
-    let displayGames = gameArticles.map(function (games) {
-      console.log(games);
-      let genre = games.genres.map(function (element) {
-        return element.name;
-      });
+  .then(function (platform) {
+    // console.log(platform);
+    // request games
+    const request = fetch(
+      "https://api.rawg.io/api/games?platforms=166&key=73601ec88eab474386a6952aa8b34734"
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        const gameArticles = data.results;
+        let displayGames = gameArticles.map(function (games) {
+          console.log(games);
+          let genre = games.genres.map(function (element) {
+            return element.name;
+          });
 
-      return `<article class="menu-item">
+          return `<article class="menu-item">
           <img class="photo" src="${games.background_image}" />         
     <div class="item-info">
     <header class='meta'>
@@ -37,9 +45,9 @@ const request = fetch(
             </div>
     </div>
     </article>`;
-    });
+        });
 
-    displayGames = displayGames.join("");
-    sectionCenter.insertAdjacentHTML("beforeend", displayGames);
+        displayGames = displayGames.join("");
+        sectionCenter.insertAdjacentHTML("beforeend", displayGames);
+      });
   });
-// });
