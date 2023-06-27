bb09a478d4b2a758785cedbb102e7b5b0dceffc6
Tassi <angelotax@gmail.com>
2023-06-27 11:34:36 +0200

    added functionalities

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-// const platforms = fetch(
-//   'https://api.rawg.io/api/platforms?key=73601ec88eab474386a6952aa8b34734'
-// )
-//   .then(function (response) {
-//     console.log(response);
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
-const sectionCenter = document.querySelector('.section-center');
+"use strict";
+const sectionCenter = document.querySelector(".section-center");
+const ButtonContainer = document.querySelector(".btn-container");
 
 const request = fetch(
-  'https://api.rawg.io/api/games?key=73601ec88eab474386a6952aa8b34734&dates=2019-09-01,2019-09-30&platforms=18,1,'
+  "https://api.rawg.io/api/games?key=73601ec88eab474386a6952aa8b34734&dates=2019-09-01,2019-09-30&platforms=18,1,"
 )
   .then(function (response) {
     return response.json();
@@ -19,24 +11,34 @@ const request = fetch(
   .then(function (data) {
     const gameArticles = data.results;
     let displayGames = gameArticles.map(function (games) {
-      // console.log(games)
-      // const screenShots = games.short_screenshots;
-      // screenShots.map(function (images) {
-      // console.log(images);
-      // });
+      const genre = games.genres.map(function (element) {
+        console.log(element.name);
+      });
+
       return `<article class="menu-item">
           <img class="photo" src="${games.background_image}" />         
     <div class="item-info">
-    <header>
-    <h3>${games.name}</h3>
+    <header class='meta'>
+    <h4>${games.name}</h4>
     </header>
     <h4 class="price">Released ${games.released}</h4>
          <div class="underline"></div>
-          <img src="${games.image}" 
-            </img>
+         <br>
+          <div class='meta'><h3>Metacritic:<span class=rating>${
+            games.metacritic
+          }</span></h3>
+           <p class="item-text">
+              ${[genre]}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+              laboriosam excepturi! Quo, officia.
+            </p>
+            </div>
     </div>
     </article>`;
     });
-    sectionCenter.insertAdjacentHTML('beforeend', displayGames);
+
+    displayGames = displayGames.join("");
+    sectionCenter.insertAdjacentHTML("beforeend", displayGames);
   });
 // });
