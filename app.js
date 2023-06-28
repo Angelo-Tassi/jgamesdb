87778b4a935c1f5d0874ea833ec25728a488dde4
angelotax <angelotax@gmail.com>
2023-06-28 22:24:57 +0200

    Update app.js
    More data displayed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-"use strict";
-const sectionCenter = document.querySelector(".section-center");
-const ButtonContainer = document.querySelector(".btn-container");
+'use strict';
+const sectionCenter = document.querySelector('.section-center');
+const ButtonContainer = document.querySelector('.btn-container');
 // request platform
 const requestPlatform = fetch(
-  "https://api.rawg.io/api/platforms?key=73601ec88eab474386a6952aa8b34734"
+  'https://api.rawg.io/api/platforms?key=73601ec88eab474386a6952aa8b34734'
 )
   .then(function (response) {
     return response.json();
@@ -11,12 +11,12 @@ const requestPlatform = fetch(
   .then(function (platform) {
     // console.log(platform);
     // request games
-    window.addEventListener("DOMContentLoaded", function () {});
+    window.addEventListener('DOMContentLoaded', function () {});
     displayGames();
 
     function displayGames() {
       const request = fetch(
-        "https://api.rawg.io/api/games?platforms=166&key=73601ec88eab474386a6952aa8b34734"
+        'https://api.rawg.io/api/games?platforms=166&key=73601ec88eab474386a6952aa8b34734'
       )
         .then(function (response) {
           return response.json();
@@ -24,9 +24,20 @@ const requestPlatform = fetch(
         .then(function (data) {
           const gameArticles = data.results;
           let displayGames = gameArticles.map(function (games) {
+            if ((games.metacritic = 'null')) {
+              games.metacritic = 'N/A';
+            }
+
+            // if (games.metacritic.texcontent===('null')){
+            //   games.metacritic.texcontent===('N/A')
+            // }
             console.log(games);
+
             let genre = games.genres.map(function (element) {
               return element.name;
+            });
+            let platforms = games.platforms.map(function (platform) {
+              return platform.platform.name;
             });
             return `<article class="menu-item">
           <img class="photo" src="${games.background_image}" />         
@@ -35,6 +46,7 @@ const requestPlatform = fetch(
     <h4>${games.name}</h4>
     </header>
     <h4 class="price">Released ${games.released}</h4>
+    <h6>${platforms}</h6>
          <div class="underline"></div>
          <br>
           <div class='meta'><h3>Metacritic:<span class=rating>${games.metacritic}</span></h3>
@@ -47,8 +59,8 @@ const requestPlatform = fetch(
     </div>
     </article>`;
           });
-          displayGames = displayGames.join("");
-          sectionCenter.insertAdjacentHTML("beforeend", displayGames);
+          displayGames = displayGames.join('');
+          sectionCenter.insertAdjacentHTML('beforeend', displayGames);
         });
     }
   });
