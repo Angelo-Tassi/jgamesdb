2ba4e3d71bc4ae94ecdb521be172abe1265e6253
angelotax <angelotax@gmail.com>
2023-06-29 16:41:54 +0200

    code cleaned

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 const sectionCenter = document.querySelector(".section-center");
 const ButtonContainer = document.querySelector(".btn-container");
 const FilterButtons = document.querySelectorAll(".filter-btn");
+let currentPage = 10;
 
-let currentPage = 2;
 window.addEventListener("DOMContentLoaded", function () {
   displayGames();
   selectPage(currentPage);
 });
+
 function selectPage() {
   FilterButtons.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
@@ -25,29 +26,6 @@ function selectPage() {
     });
   });
 }
-// // function filterButtons()
-
-// // Function to go to the next page
-// function nextPage() {
-//   var currentPage = currentPage++;
-//   displayGames();
-// }
-
-// // Function to go to the previous page
-// function previousPage() {
-//   if (currentPage > 1) {
-//     currentPage--;
-//     displayGames();
-//   }
-// }
-// const nextButton = document.getElementById("next");
-// const previousButton = document.getElementById("previous");
-// nextButton.addEventListener("click", nextPage);
-// previousButton.addEventListener("click", previousPage);
-
-// Attach event listeners to next and previous buttons
-
-// request games
 
 function displayGames() {
   const request = fetch(
@@ -93,43 +71,3 @@ function displayGames() {
       sectionCenter.insertAdjacentHTML("beforeend", displayGames);
     });
 }
-//   const requestPlatform = fetch(
-//     'https://api.rawg.io/api/platforms?key=73601ec88eab474386a6952aa8b34734'
-//   )
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (platform) {
-//       // console.log(platform);
-//       const platforms = platform.results;
-//       let platformName = platforms.map(function (getname) {
-//         return getname.name;
-//       });
-//       // Create the select element
-//       const selectElement = document.createElement('select');
-
-//       // Create and append the default "Select Platform" option
-//       const defaultOption = document.createElement('option');
-//       defaultOption.text = 'Select Platform';
-//       selectElement.appendChild(defaultOption);
-
-//       // Loop through platform names and create options
-//       platformName.forEach(function (name) {
-//         const option = document.createElement('option');
-//         option.text = name;
-//         selectElement.appendChild(option);
-//       });
-
-//       // Append the select element to a container in the HTML document
-//       const container = document.getElementById('dropdown-container');
-//       container.appendChild(selectElement);
-//     })
-//     .catch(function (error) {
-//       // Handle any errors that occur during the fetch request
-//       console.log(error);
-//     });
-// }
-
-// }
-
-// GetPlatformName();
